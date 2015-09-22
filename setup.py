3ee65a1a4b023468121f64afb305ce12adeab53e
woodscn <woodscn@lanl.gov>
2015-09-22 09:01:21 -0600

    Modify package to use system libcuba library, rather than distributing it ourselves.

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 
-setup(name='cycuba', version='0.0.1',
+setup(name='cycuba', version='0.1.0',
       description=
       'A Cython wrapper to the Cuba library (http://www.feynarts.de/cuba/)',
       long_description=None, author='C. Nathan Woods',
-      author_email='woodscn@lanl.gov', license='LGPL',
-      install_requires=['cython'],
+      author_email='woodscn@lanl.gov', license='BSD',
       ext_modules=cythonize(
-          [Extension('_cycuba', ['_cycuba.pyx'], library_dirs=['./Cuba-4.2/'],
-                     libraries=['cuba'], include_dirs=['./Cuba-4.2/'])],
-          gdb_debug=False)
+          [Extension('_cycuba', ['_cycuba.pyx'], libraries=['cuba'])])
       )
