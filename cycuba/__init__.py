a07040a316c16843d1062e5c0a50e59ef7a32540
Nathan Woods <woodscn@lanl.gov>
2015-10-22 16:38:58 -0600

    Make CyCuba work with conda installs.

--- cycuba/__init__.py
+++ cycuba/__init__.py
@@ -1,3 +1,4 @@
 from .cycuba import Vegas, Suave, Divonne, Cuhre, test
 from .cycuba import CyCubaError, CyCubaWarning
+from .cycuba import CyCubaIntegration, integer_bit_flags
 from .tests.test_cycuba import TestIntegrand
