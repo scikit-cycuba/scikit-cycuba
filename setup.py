49988c7a0e1783b4b43ac36ab5e9c53e35b246fb
Nathan Woods <woodscn@lanl.gov>
2016-02-19 14:11:47 -0700

    Make CyCuba Python2 compatible.

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setup(name='cycuba', version='0.1.0',
       'A Cython wrapper to the Cuba library (http://www.feynarts.de/cuba/)',
       long_description=None, author='C. Nathan Woods',
       author_email='woodscn@lanl.gov', license='BSD',
-      ext_modules=extensions, packages = find_packages()
+      ext_modules=extensions, packages = find_packages(),
+      zip_safe=False
       )
